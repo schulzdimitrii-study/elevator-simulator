754a6b2c26722d489c3b9688f90257452b24f63a
Dimitri <dimitri.s.amado@gmail.com>
2025-10-15 11:38:52 -0300

    refactor: Update type hints and improve code readability in Elevator class and API routes

--- app/elevator.py
+++ app/elevator.py
@@ -1,4 +1,6 @@
 import time
+from typing import Dict
+
 
 class Elevator:
     def __init__(self, name, passengers_pool, log_pool, motorLock=None, sync_mode=True):
@@ -6,16 +8,16 @@ class Elevator:
         self.current_floor = 0
         self.target_floor = 0
         self.moving = False
-        self.direction = 'stopped'
+        self.direction = "stopped"
         self.passengers_pool = passengers_pool  # pool compartilhado
         self.log_pool = log_pool  # log compartilhado
         self.current_passenger = None
         self.motorLock = motorLock
         self.sync_mode = sync_mode
 
-    def useMotor(self, destinys):
+    def useMotor(self, destinys) -> None:
         """Recurso compartilhado entre os elevadores"""
-        
+
         for destiny in destinys:
             while self.current_floor != destiny:
                 if self.current_floor < destiny:
@@ -24,7 +26,7 @@ class Elevator:
                     self.move_down()
                 time.sleep(1)
 
-    def elevator_thread(self):
+    def elevator_thread(self) -> None:
         while True:
 
             if self.current_floor > 0:
@@ -35,7 +37,11 @@ class Elevator:
 
             passenger = None
             for p in self.passengers_pool:
-                if not p["in_elevator"] and not p["is_arrived"] and p["current_floor"] == 0:
+                if (
+                    not p["in_elevator"]
+                    and not p["is_arrived"]
+                    and p["current_floor"] == 0
+                ):
                     passenger = p
                     break
 
@@ -49,16 +55,18 @@ class Elevator:
                     raise SystemExit
                 time.sleep(1)
 
-    def _transport_passenger(self, passenger):
+    def _transport_passenger(self, passenger) -> None:
         """Ciclo completo:
         Leva o passageiro ao andar destino e retorna"""
-        
+
         passenger["in_elevator"] = True
         passenger["current_floor"] = self.current_floor
         target = passenger["destiny_floor"]
         self.target_floor = target
 
-        self.log_pool.append(f"Elevador {self.name} pegou {passenger['name']} no térreo e vai para {target}")
+        self.log_pool.append(
+            f"Elevador {self.name} pegou {passenger['name']} no térreo e vai para {target}"
+        )
 
         if self.sync_mode and self.motorLock:  # Modo sincronismo
             self.log_pool.append(f"Elevador {self.name} aguardando motor")
@@ -73,22 +81,26 @@ class Elevator:
                 passenger["is_arrived"] = True
                 self.current_floor = target
                 self.target_floor = self.current_floor
-                self.log_pool.append(f"Elevador {self.name} deixou {passenger['name']} no andar {target}")
+                self.log_pool.append(
+                    f"Elevador {self.name} deixou {passenger['name']} no andar {target}"
+                )
 
                 self.useMotor([0])
                 self.current_floor = 0
                 self.target_floor = 0
                 self.direction = "stopped"
-                self.log_pool.append(f"Elevador {self.name} retornou ao térreo e liberou o motor")
+                self.log_pool.append(
+                    f"Elevador {self.name} retornou ao térreo e liberou o motor"
+                )
                 time.sleep(2)
-        else:   # Moem sincronismo
+        else:  # Modo sem sincronismo
             self.useMotor([target])
             passenger["in_elevator"] = False
             passenger["current_floor"] = target
             passenger["is_arrived"] = True
             self.useMotor([0])
 
-    def get_state(self):
+    def get_state(self) -> Dict:
         return {
             "name": self.name,
             "current_floor": self.current_floor,
@@ -97,10 +109,10 @@ class Elevator:
             "direction": self.direction,
         }
 
-    def move_down(self):
+    def move_down(self) -> None:
         self.current_floor -= 1
         self.direction = "down"
 
-    def move_up(self):
+    def move_up(self) -> None:
         self.current_floor += 1
         self.direction = "up"
--- routes/elevators.py
+++ routes/elevators.py
@@ -1,13 +1,16 @@
-from flask import Blueprint, render_template, jsonify, request
-from app.simulation import Simulation
 import ast
 
-elevators_bp = Blueprint('elevators_bp', __name__)
+from flask import Blueprint, jsonify, render_template, request
+
+from app.simulation import Simulation
+
+elevators_bp = Blueprint("elevators_bp", __name__)
 
 simulation = Simulation()
 
+
 # 1 Elevator Simulation Endpoints --------------------------------
-@elevators_bp.route('/api/state_1', methods=['GET'])
+@elevators_bp.route("/api/state_1", methods=["GET"])
 def get_state_1():
     sim = simulation.ensure_simulation(1)
     elev = sim["elevators"][0]
@@ -17,10 +20,10 @@ def get_state_1():
     return jsonify(state)
 
 
-@elevators_bp.route('/api/reset_1', methods=['POST'])
+@elevators_bp.route("/api/reset_1", methods=["POST"])
 def reset_1():
-    sync = request.args.get('sync', 'on').lower() == 'on'
-    sort_passengers_by_priority = ast.literal_eval(request.args.get('sort_by_priority'))
+    sync = request.args.get("sync", "on").lower() == "on"
+    sort_passengers_by_priority = ast.literal_eval(request.args.get("sort_by_priority"))
     sim = simulation.reset_simulation(1, sort_passengers_by_priority, sync)
     elev = sim["elevators"][0]
     state = elev.get_state()
@@ -29,17 +32,17 @@ def reset_1():
     return jsonify(state)
 
 
-@elevators_bp.route('/api/start_1', methods=['POST'])
+@elevators_bp.route("/api/start_1", methods=["POST"])
 def start_1():
-    sync = request.args.get('sync', 'on').lower() == 'on'
-    sort_passengers_by_priority = ast.literal_eval(request.args.get('sort_by_priority'))
+    sync = request.args.get("sync", "on").lower() == "on"
+    sort_passengers_by_priority = ast.literal_eval(request.args.get("sort_by_priority"))
     simulation.start_simulation(1, sort_passengers_by_priority, sync)
-    
+
     return jsonify({"message": f"Simulation Started (sync={'on' if sync else 'off'})"})
 
 
 # 2 Elevator Simulation Endpoints --------------------------------
-@elevators_bp.route('/api/state_2', methods=['GET'])
+@elevators_bp.route("/api/state_2", methods=["GET"])
 def get_state_2():
     sim = simulation.ensure_simulation(2)
     resp_data = {
@@ -50,26 +53,26 @@ def get_state_2():
     return jsonify(resp_data)
 
 
-@elevators_bp.route('/api/reset_2', methods=['POST'])
+@elevators_bp.route("/api/reset_2", methods=["POST"])
 def reset_2():
-    sync = request.args.get('sync', 'on').lower() == 'on'
-    sort_passengers_by_priority = ast.literal_eval(request.args.get('sort_by_priority'))
+    sync = request.args.get("sync", "on").lower() == "on"
+    sort_passengers_by_priority = ast.literal_eval(request.args.get("sort_by_priority"))
     simulation.reset_simulation(2, sort_passengers_by_priority, sync)
 
     return jsonify({"message": f"Simulation Reset (sync={'on' if sync else 'off'})"})
 
 
-@elevators_bp.route('/api/start_2', methods=['POST'])
+@elevators_bp.route("/api/start_2", methods=["POST"])
 def start_2():
-    sync = request.args.get('sync', 'on').lower() == 'on'
-    sort_passengers_by_priority = ast.literal_eval(request.args.get('sort_by_priority'))
+    sync = request.args.get("sync", "on").lower() == "on"
+    sort_passengers_by_priority = ast.literal_eval(request.args.get("sort_by_priority"))
     simulation.start_simulation(2, sort_passengers_by_priority, sync)
-    
+
     return jsonify({"message": f"Simulation Started (sync={'on' if sync else 'off'})"})
 
 
 # 4 Elevator Simulation Endpoints --------------------------------
-@elevators_bp.route('/api/state_4', methods=['GET'])
+@elevators_bp.route("/api/state_4", methods=["GET"])
 def get_state_4():
     sim = simulation.ensure_simulation(4)
     resp_data = {
@@ -80,18 +83,18 @@ def get_state_4():
     return jsonify(resp_data)
 
 
-@elevators_bp.route('/api/reset_4', methods=['POST'])
+@elevators_bp.route("/api/reset_4", methods=["POST"])
 def reset_4():
-    sync = request.args.get('sync', 'on').lower() == 'on'
-    sort_passengers_by_priority = ast.literal_eval(request.args.get('sort_by_priority'))
+    sync = request.args.get("sync", "on").lower() == "on"
+    sort_passengers_by_priority = ast.literal_eval(request.args.get("sort_by_priority"))
     simulation.reset_simulation(4, sort_passengers_by_priority, sync)
-    
+
     return jsonify({"message": f"Simulation Reset (sync={'on' if sync else 'off'})"})
 
 
-@elevators_bp.route('/api/start_4', methods=['POST'])
+@elevators_bp.route("/api/start_4", methods=["POST"])
 def start_4():
-    sync = request.args.get('sync', 'on').lower() == 'on'
-    sort_passengers_by_priority = ast.literal_eval(request.args.get('sort_by_priority'))
+    sync = request.args.get("sync", "on").lower() == "on"
+    sort_passengers_by_priority = ast.literal_eval(request.args.get("sort_by_priority"))
     simulation.start_simulation(4, sort_passengers_by_priority, sync)
     return jsonify({"message": f"Simulation Started (sync={'on' if sync else 'off'})"})
